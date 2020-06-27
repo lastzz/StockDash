f084452e4ca21b716ebc0acae4cfd84bd1375382
your name <your email>
2020-06-27 19:01:42 +0800

    第一次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <StockInfo/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StockInfo from '@/components/StockInfo.vue'
 
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
-    HelloWorld
+    StockInfo
   }
 }
 </script>
